3e5369d7aa0053b73398e6dade9b160dcc0bbaca
pfding <pfding@dd879d07-adaa-4757-b7d0-008d00b626b4>
2014-01-13 02:50:10 +0000

    IPC FW3.0 | code improve | cleanup resource definition(3nd); Description: change definition name and position, let code more clear and understandable; Risk: low; Affected Module: sys_server; Test: compiled on amba IPC; Code Review: no;
    git-svn-id: https://192.168.0.21/svn/ipc_fw3.x_core/branches/20131125@1155 dd879d07-adaa-4757-b7d0-008d00b626b4

--- modules/gmi/sys_client/source/sys_device_management_api.c
+++ modules/gmi/sys_client/source/sys_device_management_api.c
@@ -598,7 +598,7 @@ GMI_RESULT FactorySimpleDefaultAllLocal(uint16_t SessionId, uint32_t AuthValue)
             break;
         }
 
-	Result = GMI_SystemReboot(&DaemonData, GMI_MONITOR_TO_SDK_PORT_DEFAULT);
+	Result = GMI_SystemReboot(&DaemonData, GMI_DAEMON_HEARTBEAT_SDK_SERVER);
 	if (FAILED(Result))
 	{
 		SYS_CLIENT_ERROR("GMI_SystemReboot fail, Result = 0x%lx\n", Result);
@@ -644,7 +644,7 @@ GMI_RESULT FactoryDefaultAllLocal(uint16_t SessionId, uint32_t AuthValue)
 		break;
 	}
 
-	Result = GMI_SystemReboot(&DaemonData, GMI_MONITOR_TO_SDK_PORT_DEFAULT);
+	Result = GMI_SystemReboot(&DaemonData, GMI_DAEMON_HEARTBEAT_SDK_SERVER);
 	if (FAILED(Result))
 	{
 		SYS_CLIENT_ERROR("GMI_SystemReboot fail, Result = 0x%lx\n", Result);
