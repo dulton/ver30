20dcdef61d50a2dadca9ecda8001a0f6376adea3
mfhu <mfhu@dd879d07-adaa-4757-b7d0-008d00b626b4>
2014-02-14 03:44:37 +0000

    IPC FW3.0 | code migrate | migrate Ir_Cut_Adc amp media library from hwal branch; Description: merge code from 20130701 branch; Risk: low; Affected Module: sys_server; Test: compiled on amba platform; Code Review: no;
    git-svn-id: https://192.168.0.21/svn/ipc_fw3.x_core/branches/20131125@1267 dd879d07-adaa-4757-b7d0-008d00b626b4

--- prebuilt/include/media/gmi_media_ctrl.h
+++ prebuilt/include/media/gmi_media_ctrl.h
@@ -79,6 +79,71 @@ extern "C"
         uint32_t  s_AvgBitRate;
     } CVBR_Param;
 
+//3D
+	typedef enum
+	{
+		E3D_CMD_BOX_UNKNOWN = -1,
+		E3D_CMD_BOX_CLICK = 0,		//box/click command
+		E3D_CMD_MOUSE_DRAG,		    //toystick
+		E3D_CMD_ZOOM_REL,		    //relative zoom
+		E3d_CMD_EVENT_MAX = 2,		//
+	}E3dPosCmdIdx;
+
+	typedef enum
+	{
+		ERELZOOM_UNKNOWN = -1,
+		ERELZOOM_NO = 0,
+		ERELZOOM_ZOOMOUT = 1,	   //suo xiao
+		ERELZOOM_ZOOMIN = 2,	   //fang da
+		ERELZOOM_MAX = 2,
+	} ERelZoomIdx;
+
+//General Parameter
+	typedef enum
+	{
+		GEN_PARAM_INVALID = -1,
+		GEN_PARAM_AUTO	  = 0,				  //capability_auto.xml parameter
+		GEN_PARAM_IRCUT 					  //gmi_factory_setting.xml ircut parameter
+	}General_Param;
+
+	// Define CPU type
+	typedef enum 
+	{
+		E_CPU_UNKNOWN = 0,
+		E_CPU_A5S_55,
+		E_CPU_A5S_66,
+		E_CPU_A5S_88		
+	} SysCpuIdType;
+
+	// Define vin type, include sensor and decoder
+	typedef enum 
+	{
+		E_VIN_UNKNOWN = 0,
+		E_SENSOR_MN34041,
+		E_SENSOR_IMX122,
+		E_SENSOR_OV2715,
+		E_SENSOR_OV9715,
+		E_DECODER_TW9910,
+	} SysVinIdType;
+
+	// Define encoder board type
+	typedef enum 
+	{
+		E_ENCODER_BOARD_UNKNOWN = 0,
+		E_ENCODER_BOARD_NORMAL,
+		E_ENCODER_BOARD_LARK,
+	} SysEncoderBoardIdType;
+
+	// Define lens type
+	typedef enum 
+	{
+		E_ZOOM_LENS_NONE = 0,
+		E_ZOOM_LENS_DF003,
+		E_ZOOM_LENS_YB22,
+	} SysLensIdType;
+//
+
+
 	#define MAKE_MEDIA(m, c) ((m<<8)+c)
 
 	#define MEDIA_VIDEO_H264 	  MAKE_MEDIA(MEDIA_VIDEO, CODEC_H264)
@@ -260,7 +325,27 @@ extern "C"
 	#define MAX_AUDIO_AEC_DELAY   10000
 
 	//resolution ratio
-	//#define RESOLUTION_VGA_WIDTH    
+	//#define RESOLUTION_VGA_WIDTH  
+	
+	//3D
+    #define MIN_3D_POS_XY     0
+	#define MAX_3D_POS_XY     8192
+    #define MIN_3D_BOX_WH     0
+	#define MAX_3D_BOX_WH     8192
+	#define MIN_3D_PTZ_H      0
+	#define MAX_3D_PTZ_H      35999
+	#define MIN_3D_PTZ_V      0
+	#define MAX_3D_PTZ_V      9000
+
+	//PM
+	#define MIN_PM_UNIT       0
+	#define MAX_PM_UNIT       1
+	#define MIN_PM_POS        0
+	#define MAX_PM_POS        100
+	#define MIN_PM_COLOR      0
+	#define MAX_PM_COLOR      7
+	#define MIN_PM_ACTION     0
+	#define MAX_PM_ACTION     3
 
     typedef struct
     {
@@ -318,7 +403,7 @@ extern "C"
 		uint8_t s_DateStyle; //date display style, 0-year-month-day,1-month-day-year etc. default-0
 		uint8_t s_TimeStyle; //time display style, 0-hour:minute:sencond,  default-0
 		uint8_t s_FontColor; //font color type, 0-black, 1-red, 2-blue,3-green,4-yellow,5-magenta,6-cyan,7-white,8-auto(self-adaption according to backgroud,range between white and balck), default-0
-		uint8_t s_FontStyle;//Font size, range in8、 16、24、32, default-32
+		uint8_t s_FontStyle;//Font size, range in16、 24、32、40, 48default-32
 		uint8_t s_FontBlod; //0-no-blod, 1-blod, default-0
 		uint8_t s_Rotate;   //0-no-rotate, 1-rotate, default-0
 		uint8_t s_Italic;   //0-no-italic, 1-italic, default-0
@@ -334,7 +419,7 @@ extern "C"
 	{
 		uint8_t s_Flag;     //0-disable, 1-enable, default-0
 		uint8_t s_FontColor; //font color type, 0-black, 1-red, 2-blue,3-green,4-yellow,5-magenta,6-cyan,7-white,8-auto(self-adaption according to backgroud,range between white and balck), default-0
-		uint8_t s_FontStyle;//Font size, range in8、 16、24、32, default-32
+		uint8_t s_FontStyle;//Font size, range in16、 24、32、40,48, default-32
 		uint8_t s_FontBlod; //0-no-blod, 1-blod, default-0
 		uint8_t s_Rotate;   //0-no-rotate, 1-rotate, default-0
 		uint8_t s_Italic;   //0-no-italic, 1-italic, default-0
@@ -373,7 +458,7 @@ extern "C"
 		uint32_t s_BitRateDown;     //minimum bitrate of vbr, default-1000
 		uint16_t s_FrameInterval;//N of GOP, default-30
 		uint16_t s_EncodeQulity; //quality of jpeg, range from 1~100, default-50
-		uint8_t s_Rotate;         //range from 0~3, default-0
+		uint8_t s_Rotate;         //range from 0~3, default-0,1-hor,2-ver,3-180degree
 		uint8_t s_Reserved[3];
 	}VideoEncodeParam;
 
@@ -473,6 +558,76 @@ extern "C"
 		uint32_t		s_VoutFlip;      //0-AMBA_VOUT_FLIP_NORMAL, 1-AMBA_VOUT_FLIP_HV, 2-AMBA_VOUT_FLIP_HORIZONTAL,3-AMBA_VOUT_FLIP_VERTICAL, default-0
 	}VideoOutParam;
 
+//3D
+    typedef struct
+	{
+		E3dPosCmdIdx	s_CmdID;
+		int32_t         s_StreamId;         //stream ID
+		uint32_t	    s_X;				//the Xpos of top left corner
+		uint32_t    	s_Y;				//the Ypos of top left corner
+		int32_t			s_BoxWidth;			//the width of BOX
+		int32_t			s_BoxHeight;		//the height of BOX
+		uint32_t	    s_CurPtzX;          //the pos of hor of PTZ,range:0-35999
+		uint32_t	    s_CurPtzY;          //the pos of ver of PTZ,range:0-9000
+		uint32_t	    s_CurDptzX;         //the pos of hor of digital PTZ(no support)
+		uint32_t	    s_CurDptzY;         //the pos of hor of digital PTZ(no support)
+		uint32_t	    s_CurDptzZoom;		//the current DPTZ zoom, unit:0.1X(no support)
+		int32_t	        s_ZoomRel;			//relative zoom(no support)
+		int32_t			s_Reserved[6];
+	} S3dPosParamIn;
+
+    typedef  struct 
+    {
+        uint32_t	s_PanPos;			//the pos of hor of PTZ,range:0-35999
+        uint32_t	s_TiltPos;			//the pos of ver of PTZ,range:0-9000
+        uint32_t	s_ZoomPos;			//the pos of zoom
+        uint32_t	s_DptzX;            //the pos of hor of digital PTZ(no support)
+        uint32_t	s_DptzY;            //the pos of hor of digital PTZ(no support)
+        uint32_t	s_DptzZoom;			//the current DPTZ zoom, unit:0.1X(no support)
+        int32_t		s_Reserved[6];
+    }S3dPosParamOut;
+
+//PM
+	typedef struct
+	{
+		int32_t		s_RegionUnit;       //0- percent ,1-pixel
+		int32_t		s_Regionleft;       //s_RegionUnit==0:range:0-100;s_RegionUnit==1: XXpixel;
+		int32_t		s_RegionTop;        //s_RegionUnit==0:range:0-100;s_RegionUnit==1: XXpixel;
+		int32_t		s_RegionWidth;      //s_RegionUnit==0:range:0-100;s_RegionUnit==1: XXpixel;
+		int32_t		s_RegionHeight;     //s_RegionUnit==0:range:0-100;s_RegionUnit==1: XXpixel;
+		uint32_t	s_RegionColor;		// 0:Black, 1:Red, 2:Blue, 3:Green, 4:Yellow, 5:Magenta, 6:Cyan, 7:White
+		uint32_t	s_PmAction;         //0-add ,1-remove,2-replace,3-remove all
+	} PrivacyMaskParam;
+
+//General Param--capability
+	typedef struct
+	{
+		SysCpuIdType          s_CpuType;
+		SysEncoderBoardIdType s_EncodeBoardType;
+		SysVinIdType          s_SensorType;
+		SysLensIdType         s_ZoomLensType;
+		int32_t           s_Reserved;
+	}GeneralParam_Capability;
+
+	typedef struct 
+	{
+		uint32_t s_Min[2];
+		uint32_t s_Max[2]; 
+		uint32_t s_Avg[2]; 
+	} IrutCalibrationParam;
+
+	typedef struct
+	{
+		IrutCalibrationParam s_CalibrationParam[2];
+		int32_t s_DayToNightThr;  //default -15
+		int32_t s_NightToDayThr;  //default-30
+		int32_t s_IrCutMode;      //0: ae 1:adc 2:ae_ex_ir_intensity 
+		int32_t s_AdcMode;        //0: adc0 1:adc3 2:file 3:alarm in
+		int32_t s_IrLightMode;   //0: 0x83 0=open 1=close 1: 0x83 0=close 1=open
+	}GeneralParam_Ircut;
+
+//
+
 
 //media encode callback func type
     typedef void (*MediaEncCallBack)(void* UserDataPtr, MediaEncInfo*, ExtMediaEncInfo*);
@@ -560,7 +715,15 @@ extern "C"
 	GMI_RESULT GMI_AudioSetDecConfig(FD_HANDLE AudioDecHd, AudioDecParam *AudDecParamPtr);
 	GMI_RESULT GMI_AudioGetDecConfig(FD_HANDLE AudioDecHd, AudioDecParam *AudDecParamPtr);
 
+//3D
+    GMI_RESULT GMI_3DPosProcess(S3dPosParamIn *ParamIn, S3dPosParamOut *ParamOut, void *ExtDataPtr, int32_t *ExtDataType);
 
+//PM
+    GMI_RESULT GMI_PrivacyMaskSetConfig( FD_HANDLE VideoEncHd, PrivacyMaskParam* PrivacyMaskParamPtr);
+
+//General Param
+	GMI_RESULT GMI_GeneralParamSetConfig(General_Param ParamType, void_t *GeneralParamPtr);
+    GMI_RESULT GMI_GeneralParamGetConfig(General_Param *ParamType, void_t *GeneralParamPtr);
 
 #ifdef __cplusplus
 }
